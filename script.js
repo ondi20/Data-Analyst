f120254ef997d7e77f18aa93b26feccb0568114e
Onyango Ondigo <138160676+ondi20@users.noreply.github.com>
2024-05-03 15:12:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const nav = document.querySelector('.nav');
+
+toggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
